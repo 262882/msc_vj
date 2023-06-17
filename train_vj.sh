5f83317d54610b2566e5e30d67ed2191f0618b31
262882 <2628829@students.wits.ac.za>
2023-06-17 18:07:04 +0100

    Update training params

--- train_vj.sh
+++ train_vj.sh
@@ -20,12 +20,15 @@ opencv_traincascade \
 -weightTrimRate 0.95 \
 -maxDepth 1 \
 -minHitRate 0.999 \
--maxFalseAlarmRate 0.1 \
+-maxFalseAlarmRate 0.05 \
 
 mv $working_dir/cascade.xml result/ballcascade_$stages.xml
 rm -rf $working_dir
 
+# minHitRate is the parameter that ensures us that our positive training data yields at least a decent detection output
+# maxFalseAlarmRate is used to define how much features need to be added. Actually we want each weak classifier to have a very good hit rate on the positives, and then to allow them to remove negative windows, as fast as possible
 # The precision of your cascade is shown by acceptanceRatio on the last stage it should be around this value 0.000412662 or less.
 # Negatives = 2x positives
+# maxFalseAlarmRate=0.9 This means that in each stage no more than 90% of the 40 negative samples (ie 36 samples) should lie inside the boundary of positives. When the algorithm manages to put outside that boundary at least 4 samples, it can go to the next stage.
 # https://docs.opencv.org/4.x/dc/d88/tutorial_traincascade.html
 # https://answers.opencv.org/question/64431/number-of-stages-or-maxfalsealarmrate/
