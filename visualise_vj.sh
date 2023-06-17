bc644a7d342f719eeea56e6a5c97043a4836b7f4
262882 <2628829@students.wits.ac.za>
2023-06-17 18:07:04 +0100

    Update vj scripts

--- visualise_vj.sh
+++ visualise_vj.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-opencv_visualisation --image=/data/ball32.jpg --model=/data/haarcascade_frontalcatface.xml --data=/result/
+opencv_visualisation --image=./data/ball32.jpeg --model=./result/cascade.xml --data=./result/
