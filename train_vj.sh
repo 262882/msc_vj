ed8b20621400caa16c7e297dcc2f394373d5aa1e
262882 <2628829@students.wits.ac.za>
2023-06-17 18:07:04 +0100

    Tweak training parameters

--- train_vj.sh
+++ train_vj.sh
@@ -3,14 +3,20 @@ opencv_traincascade \
 -data ./result/ \
 -vec data/test.vec \
 -bg vj_train_m_False_matchdrill/neg/bg.txt \
--numPos 300 \
+-numPos 140 \
 -numNeg 280 \
 -w 28 \
 -h 28 \
 -precalcValBufSize 1024 \
 -precalcIdxBufSize 1024 \
--numStages 6 \
+-numStages 12 \
 -acceptanceRatioBreakValue 1.0e-5 \
 -mode ALL \
 -weightTrimRate 0.95 \
 -maxDepth 1 \
+-minHitRate 0.999 \
+-maxFalseAlarmRate 0.1 \
+
+# The precision of your cascade is shown by acceptanceRatio on the last stage it should be around this value 0.000412662 or less.
+# Negatives = 2x positives
+# https://answers.opencv.org/question/64431/number-of-stages-or-maxfalsealarmrate/
