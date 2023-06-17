e974c8a01fd27f75428136bf55f3fa04fd8c3148
262882 <2628829@students.wits.ac.za>
2023-06-17 18:07:04 +0100

    Update training script

--- train_vj.sh
+++ train_vj.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-opencv_traincascade -data ./results/ -vec data/test.vec -bg vj_train_m_False_matchdrill/neg/bg.txt -numPos 328 -numNeg 312 -w 28 -h 28 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 6 -acceptanceRatioBreakValue 1.0e-5
+opencv_traincascade -data ./result/ -vec data/test.vec -bg vj_train_m_False_matchdrill/neg/bg.txt -numPos 328 -numNeg 312 -w 28 -h 28 -precalcValBufSize 1024 -precalcIdxBufSize 1024 -numStages 6 -acceptanceRatioBreakValue 1.0e-5
