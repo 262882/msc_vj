52045aa6e1103be052dbcbb21a73d87d930ab69c
262882 <2628829@students.wits.ac.za>
2023-06-17 18:07:04 +0100

    Limit to 36 jobs

--- run_cluster.sh
+++ run_cluster.sh
@@ -3,9 +3,9 @@ var1="$(pwd)/results/slurm-%j.err"
 var2="$(pwd)/results/slurm-%j.out"
 var3="stampede"
 
-for numStages in 1 2 3 4 5 6 7 8 9 10 11 12
+for numStages in 2 4 6 8 10 12
 do
-    for FA in 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5
+    for FA in 0.05 0.15 0.25 0.35 0.45 0.55
     do
         sbatch --error=$var1 --output=$var2 --partition=$var3 train_vj_cluster.sh $numStages $FA &
     done
