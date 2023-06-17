ee4a831fb7b6bf4136f135a59f78efa44d2b9e50
262882 <2628829@students.wits.ac.za>
2023-06-17 18:07:04 +0100

    Continue with cluster

--- prepare_data.sh
+++ prepare_data.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 name=vj_train_m_False_matchdrill_True
-opencv_createsamples -info ./$name/pos/info.dat -w 28 -h 28 -vec data/$name.vec
+opencv_createsamples -info ./$name/pos/info.dat -w 28 -h 28 -vec data/$name.vec #-show uncomment to extract pos samples
--- train_vj_cluster.sh
+++ train_vj_cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --time=00:30:00
+#SBATCH --time=01:30:00
 #SBATCH --job-name=vjtrain
 
 stages=$1
